9aba8aedeab21b9c462ec28c313ea9912defefc0
Xavier Tordoir <xtordoir@yahoo.co.uk>
2023-12-30 19:24:19 +0100

    added merge tool from inventory

---
+++ src/bin/merge_from_inventory.rs
@@ -0,0 +1,46 @@
+extern crate gear_trading;
+
+use clap::{arg, command, Parser};
+use std::fs;
+use gear_trading::hff::agents::*;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the hedger file to merge 2 trades from
+    #[arg(short = 'f', long)]
+    hedger_file: Option<String>,
+
+    // Name of the first agent to merge
+    #[arg(short = 'n', long)]
+    name1: String,
+
+    #[arg(short = 'm', long)]
+    name2: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let name1: &str = args.name1.as_str();
+    let name2: &str = args.name2.as_str();
+
+    // read the input inventory file
+    let hedger = args
+        .hedger_file
+        .as_deref()
+        .map(|f| {
+            let hstr = fs::read_to_string(f).ok();
+            hstr.map(|s| serde_json::from_str::<AgentInventory<GearHedger>>(s.as_str()).ok())
+                .flatten()
+        })
+        .flatten().unwrap();
+
+    // extract the agent1 and agent2
+    let agent1 = hedger.agents.get(name1).unwrap();
+    let agent2 = hedger.agents.get(name2).unwrap();
+
+    let agent = agent1.merge_flat(agent2);
+
+    println!("max exposure: {}\n", agent);
+
+}
